64530829f4c7eaf38da1070d8132d96b2ad1112f
Ariel <67240342+IamAriel2@users.noreply.github.com>
2025-06-22 11:00:32 +0300

    fixed a bug

--- main.c
+++ main.c
@@ -79,31 +79,35 @@ void create_tree(Pointer *oprs[4])
         brackets_count++;
         number = brackets_handler();
     }
+    else if (*exp_ptr == '-')
+    {
+        // Handling the case where the expressions starts with a negative number.
+        // If the first character is a minus sign, we multiply the number by -1.
+        number = create_number(-1, 0);
+        *exp_ptr = '*';
+    }
     else
         number = read_number();
 
     if (number == NULL)
     {
-        // Handling the case where the expressions starts with a negative number.
-        // If the first character is a minus sign, we multiply the number by -1.
-        if (*exp_ptr == '-')
-        {
-            number = create_number(-1, 0);
-            *exp_ptr = '*';
-        }
-        else
-        {
-            oprs[0] = NULL;
-            return;
-        }
+        // If there is no number, we raise the error flag.
+        oprs[0] = NULL;
+        return;
     }
+
     // Reading the operator:
+    Pointer *operator;
     if (*exp_ptr == '\0' || *exp_ptr == ')')
     {
         oprs[0]->f.o.left = number;
         return;
     }
-    Pointer *operator = read_operator();
+    if (*exp_ptr == 'i')
+        operator = create_operator('*', NULL, NULL);
+    else
+        operator = read_operator();
+
     if (operator == NULL)
     {
         oprs[0] = NULL;
@@ -111,12 +115,12 @@ void create_tree(Pointer *oprs[4])
     }
     operator->f.o.left = number;
 
-    create_tree(oprs);
-    if (oprs[0] == 0) // Meaning that there is an error.
-        return;
-    // Linking the current operator to the tree.
-    // Finding the right place for the operator in the tree.
-    // The operator is placed in the tree according to its precedence.
+    create_tree(oprs); // We go deeper into the tree to read the next number & operator.
+    if (oprs[0] == NULL)
+        return; // If there was an error in creating the subtree, we return NULL.
+    // Linking the current operator to the subtree.
+    // Finding the right place for the operator in the subtree.
+    // The operator is placed in the subtree according to its precedence.
     char pr = is_an_opr(operator->f.o.type), i;
     for (i = pr; i >= 0 && oprs[i] == NULL; i--)
         ;
